8c4abb06640160afd9824e202dcd9ef748d3825b
Mayra Bruno <80061821+projetos-mayra@users.noreply.github.com>
2023-09-05 11:09:13 -0300

    Add files via upload

---
+++ tcc-main/tcc-main/js/script.js
@@ -0,0 +1,72 @@
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer)
+
+function startTimer() {
+
+
+    interval = setInterval(() => {
+        if (!isPaused) {
+         milliseconds += 10;
+
+        if (milliseconds === 1000) {
+            seconds++;
+            milliseconds = 0;
+        }
+
+        if(seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+
+        minutesEl.textContent = minutes
+        secondsEl.textContent = seconds
+        millisecondsEl.textContent = milliseconds
+      }
+    },  10 );
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const horas = document.getElementById('horas');
+//const minutos = document.getElementById('minutos');
+//const segundos = document.getElementById('segundos');
+
+//const relogio = setInterval(function time(){
+    //let dateToday = new Date();
+    //let hr = dateToday.getHours();
+    //let min = dateToday.getMinutes();
+   // let s = dateToday.getSeconds();
+
+   // horas.textContent = hr;
+    //minutos.textContent = min;
+    //segundos.textContent = s;
+    
+//})
